7d3f0236852c2f367bf201c6f54cc50ad5506243
senthil nayagam <senthil.nayagam@gmail.com>
2020-05-25 13:45:57 +0530

    opensourcing my first project on FossterCare

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # flask-pgn-api
 python flask api to convert chess single game pgn to mp4 videos and fen to png images
+
+
+
+install all dependencies
+pip install -r requirements.txt
+
+
+
+running app in development mode
+export FLASK_APP=pgn-api.py
+$ python -m flask run
+ * Running on http://127.0.0.1:5000/
+
+runing app in production mode
+gunicorn --bind 0.0.0.0:5000 -w 4  pgn-api:app
+
+use procfile if possible
+
+
+rendering fen as svg and png
+
+ http://127.0.0.1:5000/svg/2b3k1/8/2pqp3/p1p2r2/2PbN3/3PQN2/PP3PP1/4R1K1%20b%20-%20-%208%2035
+
+ http://127.0.0.1:5000/png/2b3k1/8/2pqp3/p1p2r2/2PbN3/3PQN2/PP3PP1/4R1K1%20b%20-%20-%208%2035
+
+
+
+
+
+
+pip install httpie
+brew install httpie
+
+http --download -f POST http://127.0.0.1:5000/pgn/  file@file.pgn
+
+where file.pgn is the actual filename for single game
+
+you can split a multi game pgn to multiple single game pgn with pgn-extract
---
+++ pgn-api.py
@@ -0,0 +1,132 @@
+import chess
+import chess.pgn
+import chess.svg
+import cairosvg
+
+from flask import Flask, Response, flash, request, redirect, url_for, send_file
+from werkzeug.utils import secure_filename
+import os
+import io
+
+#import tempfile
+import cv2
+import uuid
+import numpy as np
+
+UPLOAD_FOLDER = 'tmp'
+ALLOWED_EXTENSIONS = {'pgn' }
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+
+@app.route('/png/<path:path>')
+def render_png(path):
+	board = chess.Board(path)
+	svg = chess.svg.board(board=board)
+	png_data2x = cairosvg.svg2png(bytestring=svg,output_height=800,output_width=800)
+	return Response(png_data2x,mimetype='image/png')
+
+
+
+@app.route('/svg/<path:path>')
+def render_svg(path):
+	board = chess.Board(path)
+	svg = chess.svg.board(board=board)
+	return Response(svg,mimetype='image/svg+xml')
+
+
+@app.route('/<fen>')
+def render_fen2(fen):
+	return 'fen'
+
+
+
+def pgn_to_mp4(pgn_filename,uuid_filename):
+	#pgn = io.StringIO(pgn_upload_filename.read().decode("utf-8") )
+	pgn = open(pgn_filename)
+	first_game = chess.pgn.read_game(pgn)
+	board = first_game.board()
+	frame_array = []
+	fps = 1
+	index = 0
+	#uuid_filename = str(uuid.uuid4())
+
+	pathOut = "tmp/" + uuid_filename + ".mp4" #mp4_filename
+	width = 800
+	height = 800
+	size = (width,height)
+	out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'MP4V'), fps, size)
+
+	index = 1
+
+	for move in first_game.mainline_moves():
+		board.push(move)
+		print(move)
+		print(index)
+		data = chess.svg.board(board=board)
+		##png_data = cairosvg.svg2png(bytestring=data)
+		png_data2x = cairosvg.svg2png(bytestring=data,output_height=800,output_width=800)
+		nparr = np.fromstring(png_data2x, np.uint8)
+		img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR) 
+		frame_array.append(img_np)
+		#	file_write(index,board,new_directory )
+		index += 1
+	
+	print(len(frame_array))
+	for i in range(len(frame_array)):
+		# writing to a image array
+		out.write(frame_array[i])
+
+	out.release()
+	return pathOut
+
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/pgn/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            uuid_filename = str(uuid.uuid4())
+            pgn_filename= os.path.join(app.config['UPLOAD_FOLDER'], uuid_filename + '.pgn')
+            file.save(pgn_filename)
+            
+            mp4_file = pgn_to_mp4(pgn_filename,uuid_filename)
+            print(mp4_file)
+
+            return send_file(mp4_file,mimetype="video/mp4",as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Upload PGN File with one game</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+
+if __name__ == "__main__":
+	app.run()
